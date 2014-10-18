d45f9d0d3d8b70744e5a669e950969c03a95300b
YiChenshu <ycswaves@gmail.com>
2014-10-18 20:29:05 +0800

    ios training camp 1st lesson

--- Fortuna/AppDelegate.swift
+++ Fortuna/AppDelegate.swift
@@ -12,11 +12,33 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    
+    var positiveQuotes: [String]!
+    var negativeQuotes: [String]!
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        positiveQuotes = loadJSON(NSBundle.mainBundle().pathForResource("positiveQuotes", ofType: "json")!) as [String]!
+        
+        negativeQuotes = loadJSON(NSBundle.mainBundle().pathForResource("negativeQuotes", ofType: "json")!) as [String]!
+        
+        // Assertions to make sure that the quotations are loaded.
+        assert(positiveQuotes.count > 0, "should load positive quotes")
+        assert(negativeQuotes.count > 0, "should load negative quotes")
         return true
+    }
+    
+    func loadJSON(path: String) -> AnyObject? {
+        // Load data from path
+        let data = NSData(contentsOfFile: path)
+        assert(data != nil, "Failed to read data from: \(path)")
+        
+        // Parse JSON data
+        var err : NSError?
+        let json: AnyObject? = NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.allZeros, error: &err)
+        assert(err == nil, "Error parsing json: \(err)")
+        
+        return json
     }
 
     func applicationWillResignActive(application: UIApplication) {
@@ -40,7 +62,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
 }
 
--- Fortuna/ViewController.swift
+++ Fortuna/ViewController.swift
@@ -10,16 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func rootViewTapped(sender: UITapGestureRecognizer) {
+        displayRandomQuote()
+    }
+    @IBOutlet weak var quotationTextView: UITextView!
+    let delegate = UIApplication.sharedApplication().delegate as AppDelegate
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        quotationTextView.editable = false
+        quotationTextView.selectable = false
+        displayRandomQuote()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func displayRandomQuote() {
+        let posOrNeg = arc4random_uniform(2)
+        var quotes: [String]
+        if posOrNeg == 1 {
+            quotes = delegate.positiveQuotes
+        } else {
+            quotes = delegate.negativeQuotes
+        }
+        let quote = quotes[Int(arc4random_uniform(UInt32(quotes.count)))]
+        quotationTextView.text = quote
+    }
 
 }
 
